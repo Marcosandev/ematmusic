4246af20e8bcc6ca7bb7ad06e1f176e41f4039bb
Marcosandev <marcoasanchezg2507@gmail.com>
2026-02-21 11:18:27 -0400

    asd

--- src/models/preferences.py
+++
@@ -1,8 +0,0 @@
-class UserSettings:
-    def __init__(self, id: int = None, dark_mode: bool = False, language: str = "en"):
-        self.id = id
-        self.dark_mode = dark_mode
-        self.language = language
-
-    def __repr__(self):
-        return f"<UserSettings(id={self.id}, dark_mode={self.dark_mode}, language='{self.language}')>"
--- src/views/theme.py
+++
@@ -1,9 +0,0 @@
-import flet as ft
-
-COLOR_TITULO = ft.Colors.BLUE_ACCENT_100
-COLOR_FONDO = ft.Colors.WHITE
-FUENTE_TITULO = ft.TextStyle(
-    size=40,
-    weight=ft.FontWeight.BOLD,
-    color=COLOR_TITULO,
-)
